ea718227a5c5382756bfe227465e5bce6cee0d1f
kirankumar <new email>
2023-02-26 22:49:07 +0530

    first commit

---
+++ testapp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def page_count_view(request):
+    print(request.COOKIES)
+
+    count = request.session.get('count',0)
+    count = count + 1
+
+    request.session['count'] = count
+    # request.session.set_expiry(120)
+    print(request.session.get_expiry_age())
+    print(request.session.get_expiry_date())
+
+    return render(request,'testapp/pagecount.html',{'count':count})
